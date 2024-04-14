8f4389f6e2be843889612a6882dde094528ce05a
illTushar <gtushar7668@gmail.com>
2024-04-14 22:41:10 +0530

    path_parameter

--- fast_api_app.py
+++ fast_api_app.py
@@ -4,12 +4,17 @@ from request.product import Item
 app = FastAPI()
 
 
+# path parameter
+@app.get("/item/{id}")
+async def item_id(id):
+    return {"message": f"This item_id is {id}"}
+
+
 @app.post("/product")
 async def item_details(item: Item):
     item_new_key = item.dict()
 
     if item.price is not None and item.gst is not None:
-    
         include_gst_price = item.price + item.gst
         item_new_key["gst_include_price"] = include_gst_price
 
